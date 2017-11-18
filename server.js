0b56781f14f9867f5629882d4e3e346ff40272e7
danschroeder <dankschroeder@gmail.com>
2017-11-18 11:14:28 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+var https = require("https");
+var http = require('http');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
