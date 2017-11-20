b90ba3d400f37d1522aa5da0c78e585730d2e7a4
dan-schroeder <dan.schroeder@domo.com>
2017-11-20 16:46:57 -0700

    working oauth

--- server.js
+++ server.js
@@ -8,14 +8,22 @@ const canvasConfig = {
 const PORT = 3000;
 const HOST = 'localhost';
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost');
+var Schema = mongoose.Schema;
+var User = require('./models/user.js');
+
+
+var request = require("request");
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-var User = require('./models/user.js');
+
 //const https = require("https");
 //const http = require('http');
 //const expressVue = require('express-vue');
-const passport = require('passport'),OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+const passport = require('passport'),
+    OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
 const helmet = require('helmet');
 const app = express();
 app.use(express.static("public"));
@@ -32,68 +40,71 @@ app.use(passport.session());
 
 
 let oauthClient = new OAuth2Strategy({
-    authorizationURL: canvasConfig.baseUrl + '/login/oauth2/auth',
-    tokenURL: canvasConfig.baseUrl + '/login/oauth2/token',
-    clientID: canvasConfig.clientID,
-    clientSecret: canvasConfig.clientSecret,
-    callbackURL: `http://${HOST}:${PORT}/auth/provider/callback`,
-    passReqToCallback: true,
-    profileFields: ['id','name','user']
-},
-function (req, accToken, refToken, profile, done) {
-    //check user table for anyone with a userId of profile.id
-    console.log('info returned');
-    console.log(accToken);
-    console.log(refToken);
-    console.log(profile);
-    
-    User.findOne({
-        'userId': profile.id
-    }, function (err, user) {
-        if (err) {
-            console.log("error: "+err);
-            return done(err);
-        }
-        //No user was found... so create a new user with values from canvas (all the profile. stuff)
-        if (!user) {
-            console.log('no user found creating user...')
-            user = new User({
-                userId: profile.id.toString(),
-                name: profile.name,
-                accessToken: accToken,
-                refreshToken: refToken,
-                provider: authProvider
-            });
-            user.save(function (err) {
-                console.log('saving user to db...');
-                if (err) console.log(err);
-                return done(err, user);
-            });
-        } else {
-            //found user. Return
-            return done(err, user);
-        }
-    });
-});
-oauthClient.userProfile = function (accesstoken, done) {
-    // choose your own adventure, or use the Strategy's oauth client
-    this._oauth2._request("GET", canvasConfig.baseUrl + '/api/v1/users/self', null, null, accesstoken, (err, data) => {
-      if (err) { return done(err); }
-      try {
-            console.log(data.toString());
-          data = JSON.parse( data );
-          console.log(Object.prototype.toString.call(data));
-          
-      }
-      catch(e) {
-          console.log('error');
-        return done(e);
-      }
-      done(null, data);
-    });
-  };
+        authorizationURL: canvasConfig.baseUrl + '/login/oauth2/auth',
+        tokenURL: canvasConfig.baseUrl + '/login/oauth2/token',
+        clientID: canvasConfig.clientID,
+        clientSecret: canvasConfig.clientSecret,
+        callbackURL: `http://${HOST}:${PORT}/auth/canvas/callback`
+    },
+    function (accToken, refToken, profile, done) {
+        //check user table for anyone with a userId of profile.id
+        console.log('info returned');
+        console.log(accToken);
+        console.log(refToken);
+        console.log(profile.id);
 
-passport.use(authProvider,oauthClient);
+        User.findOne({
+            userId: profile.id
+        }, function (err, user) {
+            if (err) {
+                console.log("error: " + err);
+                return done(err);
+            }
+            //No user was found... so create a new user with values from canvas (all the profile. stuff)
+            if (!user) {
+                console.log('no user found creating user...');
+                user = new User({
+                    userId: profile.id,
+                    name: profile.name,
+                    accessToken: accToken,
+                    refreshToken: refToken,
+                    provider: authProvider
+                });
+                user.save(function (err) {
+                    console.log('saving user to db...');
+                    if (err) console.log(err);
+                    return done(err, user);
+                });
+            } else {
+                //found user. Return
+                return done(err, user);
+            }
+        });
+
+    });
+oauthClient.userProfile = function (accessToken, done) {
+    // choose your own adventure, or use the Strategy's oauth client
+    getUserInfo(accessToken, function(data){
+        try {
+            console.log(data);
+            data = JSON.parse(data);
+
+        } catch (e) {
+            console.log('error');
+            return done(e);
+        }
+        done(null, data);
+    });        
+};
+
+passport.use(authProvider, oauthClient);
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
 
 
 //######## END OF CONFIG STUFF ######
@@ -102,20 +113,27 @@ passport.use(authProvider,oauthClient);
 
 app.use(helmet());
 //app.use(expressVueMiddleware);
-app.get('/auth/provider', passport.authenticate(authProvider));
-app.get('/auth/provider/callback', passport.authenticate(authProvider, {
+app.get('/auth/canvas', passport.authenticate(authProvider, {scope: 'auth/userinfo'}));
+app.get('/auth/canvas/callback', passport.authenticate(authProvider, {
     successRedirect: '/home',
     failureRedirect: '/login'
 }));
 // app.get('/auth/provider/callback',(req, res) => {
 //     res.send();
 // });
+app.all('*',(req, res, next) =>{
+    if(typeof req.user === "undefined"){
+        res.redirect('/auth/canvas');
+    }
+    next();
+});
 
 app.get('/', (req, res) => {
-    res.send('Hello<br><a href="/auth/provider">Log in with Canvas</a>');
+    console.log(req.user);
+    res.send('Hello<br><a href="/auth/canvas">Log in with Canvas</a>');
 });
 app.get('/login', (req, res) => {
-    res.send('Hello<br><a href="/auth/provider">Log in with Canvas</a>');
+    res.send('Hello<br><a href="/auth/canvas">Log in with Canvas</a>');
 });
 
 app.get('/home', (req, res) => {
@@ -128,8 +146,18 @@ app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
 //######## UTILITY FUNCTIONS ##########
-function getUserInfo(){
+function getUserInfo(token,callback) {
+    var options = { method: 'GET',
+    url: 'https://utah-valley-university.acme.instructure.com/api/v1/users/self',
+    headers: 
+     { 'cache-control': 'no-cache',
+       authorization: 'Bearer '+token } };
+  
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+  
+    console.log(body);
+    callback(body);
+  });
 
 }
-
-
--- user.js
+++ user.js
@@ -1,13 +1,20 @@
 var mongoose = require('mongoose'),
 Schema = mongoose.Schema,
 ObjectId = Schema.ObjectId;
+mongoose.connect('mongodb://localhost');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
 
 var userSchema = new Schema({
 provider:  String,
 accessToken: String,
 refreshToken: String,
 name: String,
-userId: {type: ObjectId, ref: 'User'},
+userId: String,
+id: {type: ObjectId, ref: 'User'},
 dateAdded: {type: Date, default: Date.now}
 });
 
